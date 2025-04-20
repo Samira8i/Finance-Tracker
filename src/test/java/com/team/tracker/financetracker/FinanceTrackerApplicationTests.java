a0e85b4eb3d11ab2f2df84caba7cafb4ccbf4a96
a4ee <ilya.pozdnyakov.04@gmail.com>
2025-04-20 18:05:00 +0300

    Init project

---
+++ src/test/java/com/team/tracker/financetracker/FinanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.tracker.financetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
