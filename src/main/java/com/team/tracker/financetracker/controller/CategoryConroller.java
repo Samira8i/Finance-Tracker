42f1e8b86c6c3ce4dcb7b765331ebc11e16b764c
kkruchock <mosk.bima@gmail.com>
2025-05-11 21:06:59 +0300

    TF-15
    Commit for merge

--- src/main/java/com/team/tracker/financetracker/controller/CategoryConroller.java
+++ src/main/java/com/team/tracker/financetracker/controller/CategoryConroller.java
@@ -23,5 +23,4 @@ public class CategoryConroller {
     public List<Category> getCategoriesByType(@RequestParam CategoryType categoryType) {
         return categoryService.getCategoriesByType(categoryType);
     }
-
 }
