df39b35cc73f736d2a632431b4f7d4957d599aab
kkruchock <mosk.bima@gmail.com>
2025-05-17 14:47:49 +0300

    TF-22
    -фикс ошибки в transactionsRepository
    -изменение контроллеров, для дальнейшей связи с фронтом

--- src/main/java/com/team/tracker/financetracker/controller/CategoryConroller.java
+++ src/main/java/com/team/tracker/financetracker/controller/CategoryConroller.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/categories")
+@RequestMapping("/api/v1/categories")
+@CrossOrigin(origins = "http://localhost:63342")
 public class CategoryConroller {
 
     private final CategoryService categoryService;
